0b96e5e4b574b8cd26fe1cec964e353064fd7ce7
Giovanni Zaccaria <giozaccaria02@gmail.com>
2024-08-27 15:09:18 +0200

    refactor main.c

--- main.c
+++ main.c
@@ -4,14 +4,8 @@
 int main() {
     struct points* points = points_linit(true, 8, 0, 3, 1, 1, 2, 4, 3, 9);
 
-    // for (size_t i = 0; i < points->length; i++) {
-    //     fprintf(stdout, "%ld: ", i);
-    //     point_display(points->list[i], stdout);
-    //     fputs("\n", stdout);
-    // }
-
     fprintf(stdout, "x, y\n");
-    for (double x = 0.0; x < 8.0; x += 0.01) {
+    for (double x = 0.0; x <= 7.0; x += 0.01) {
         double y = lagrange_interpolation(points, x);
         fprintf(stdout, "%.4f, %.4f\n", x, y);
     }
